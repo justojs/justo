fcf401fb2c869ddf23015dca78b2efbd27b63dc4
Raúl G. González <raulggonzalez@justojs.org>
2016-05-18 09:46:07 +0200

    Fix #18

--- dist/es5/nodejs/justo/index.js
+++ dist/es5/nodejs/justo/index.js
@@ -17,22 +17,25 @@ Object.defineProperty(justo, "initialize", { value: initialize });
 
 
 function init(config) {
+  var Console = require("justo-console");
   var log = require("justo-logger");
   var rep = require("justo-reporter");
   var Runner = require("justo-runner").Runner;
-  var loggers, reporters, runner;
+  var console, loggers, reporters, runner;
 
+
+  console = new Console();
 
   loggers = new log.Loggers();
   loggers.add(new log.logger.ColoredConsoleLogger(config.runner.logger));
 
   reporters = new rep.Reporters();
-  if (!config.reporter) reporters.add(new rep.reporter.ColoredConsoleReporter(config.reporter));else 
-  if (config.reporter.type == "console") reporters.add(new rep.reporter.ConsoleReporter(config.reporter.console));else 
-  reporters.add(new rep.reporter.ColoredConsoleReporter(config.reporter.coloredConsole));
+  if (!config.reporter) reporters.add(new rep.reporter.ColoredConsoleReporter(Object.assign({ console: console }, config.reporter)));else 
+  if (config.reporter.type == "console") reporters.add(new rep.reporter.ConsoleReporter(Object.assign({ console: console }, config.reporter.console)));else 
+  reporters.add(new rep.reporter.ColoredConsoleReporter(Object.assign({ console: console }, config.reporter.coloredConsole)));
   reporters.add(new rep.reporter.StateReporter("state"));
 
 
-  runner = new Runner({ loggers: loggers, reporters: reporters, only: !!config.runner.only });
+  runner = new Runner({ loggers: loggers, reporters: reporters, console: console, only: !!config.runner.only });
   Object.defineProperty(justo, "runner", { value: runner });
   runner.publishInto(justo);}
--- index.js
+++ index.js
@@ -17,23 +17,26 @@ Object.defineProperty(justo, "initialize", {value: initialize});
  * Initializes Justo.js.
  */
 function init(config) {
+  const Console = require("justo-console");
   const log = require("justo-logger");
   const rep = require("justo-reporter");
   const Runner = require("justo-runner").Runner;
-  var loggers, reporters, runner;
+  var console, loggers, reporters, runner;
 
-  //(1) create loggers and reporters used by the runners
+  //(1) create console, loggers, reporters used by the runners
+  console = new Console();
+
   loggers = new log.Loggers();
   loggers.add(new log.logger.ColoredConsoleLogger(config.runner.logger));
 
   reporters = new rep.Reporters();
-  if (!config.reporter) reporters.add(new rep.reporter.ColoredConsoleReporter(config.reporter));
-  else if (config.reporter.type == "console") reporters.add(new rep.reporter.ConsoleReporter(config.reporter.console));
-  else reporters.add(new rep.reporter.ColoredConsoleReporter(config.reporter.coloredConsole));
+  if (!config.reporter) reporters.add(new rep.reporter.ColoredConsoleReporter(Object.assign({console}, config.reporter)));
+  else if (config.reporter.type == "console") reporters.add(new rep.reporter.ConsoleReporter(Object.assign({console}, config.reporter.console)));
+  else reporters.add(new rep.reporter.ColoredConsoleReporter(Object.assign({console}, config.reporter.coloredConsole)));
   reporters.add(new rep.reporter.StateReporter("state"));
 
   //(2) create default automator
-  runner = new Runner({loggers, reporters, only: !!config.runner.only});
+  runner = new Runner({loggers, reporters, console, only: !!config.runner.only});
   Object.defineProperty(justo, "runner", {value: runner});
   runner.publishInto(justo);
 }
